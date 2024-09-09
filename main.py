b0242e00ac4fefb557ecc299d4450da7f39374ab
RadunTheSniffer <ridhuan.work2001@gmail.com>
2024-09-09 07:07:23 +0000

    fixed error

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-from fastapi import Request
-
 
 app = FastAPI()
 
 # Mount static files
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-#  Set up Jinja templates directory
-templates = Jinja2Templates
+# Set up Jinja templates directory
+templates = Jinja2Templates(directory="templates")
 
 # Example endpoint that renders a Jinja template
 @app.get("/dashboard/")
@@ -19,5 +17,6 @@ def render_dashboard(request: Request):
         {"name": "Task 1", "time_taken": 3.5, "difficulty": 2},
         {"name": "Task 2", "time_taken": 5.0, "difficulty": 4},
     ]
-    return templates.TemplateResponse("dashbaord.html", {"request": request, "tasks": tasks})
+    return templates.TemplateResponse("dashboard.html", {"request": request, "tasks": tasks})
+
 
